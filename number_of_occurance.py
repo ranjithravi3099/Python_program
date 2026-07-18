205299035156d4955b7b08dc6094a034df7eb8ba
ranjithravi3099 <ranjithravi3099@gmail.com>
2026-07-18 06:20:25 +0530

    Add character occurrence counting in a string & find duplicate

--- number_of_occurance.py
+++ number_of_occurance.py
@@ -11,3 +11,17 @@ print(a)
 for i,j in a.items():
     if a[i] <= 1:
         print(i,"-",a[i])
+
+# without using dict
+a = "banana"
+b = ""
+
+for i in a:
+    if i is not b:
+        count = 0
+        
+        for j in b:
+            if i ==j:
+                count = count + 1
+    print(i,count)
+    b = b + i
